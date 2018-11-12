18fb71f3f012330079e983b0d6cef1d4ac1877fc
agathe boucher <aboucher@lyceeTech.ledantec.fr>
2018-11-12 11:53:22 +0100

    Major update

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const superagent = require('superagent');
+var JsonDB = require('node-json-db');
+var db = new JsonDB("db", true, false);
+
+let response;
+
+setInterval(async () => {
+	console.log("Store data !");
+	await superagent
+	  .get('https://raw.githubusercontent.com/lucienbl/weather-station-api-response-simulator/master/api.json')
+	  .set('accept', 'json')
+	  .end((err, data) => {
+	    response = JSON.parse(data.text);
+	  });
+	db.push(`/${Date.now()}`, response);
+}, 600000);
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send(db.getData('/'));
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('App listening on port 3000!')
+});
